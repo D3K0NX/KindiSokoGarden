1f5d0599c798b785ef2d3e0c8299122dd8cab347
D3K0NX <d3k0ngang@gmail.com>
2026-04-17 13:40:42 +0300

    uwd

--- app/src/main/java/com/example/kindisokogarden/AboutActivity.kt
+++ app/src/main/java/com/example/kindisokogarden/AboutActivity.kt
@@ -1,9 +1,13 @@
 package com.example.kindisokogarden
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.speech.tts.TextToSpeech
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +16,8 @@ import java.util.Locale
 
 class AboutActivity : AppCompatActivity() {
     lateinit var tts: TextToSpeech
+    //variable for speach text
+    lateinit var speechText: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,6 +30,17 @@ class AboutActivity : AppCompatActivity() {
 
         val textView=findViewById<TextView>(R.id.tv_about)
         val speakButton=findViewById<Button>(R.id.speak_btn)
+
+
+        //views for speach to text
+        speechText=findViewById<TextView>(R.id.tv_speech)
+        val speechButton=findViewById<Button>(R.id.tap_speak_btn)
+
+        //
+        speechButton.setOnClickListener {
+            //start speaking
+            speakNow()
+        }
 
         // create tts object
         tts= TextToSpeech(this){
@@ -42,6 +59,8 @@ class AboutActivity : AppCompatActivity() {
             tts.speak(text, TextToSpeech.QUEUE_FLUSH,null,null)
         }
 
+
+
     }
 
     //this function is executed when the activity/app is closed/destroyed/killed
@@ -49,5 +68,29 @@ class AboutActivity : AppCompatActivity() {
         tts.stop()
         tts.shutdown()
         super.onDestroy()
+    }
+
+
+    //
+    private fun speakNow() {
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak now...")
+
+        try {
+            startActivityForResult(intent, 100)
+        } catch (e: Exception) {
+            Toast.makeText(this, "Speech recognition not supported", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK && data != null) {
+            val result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            speechText.text = result?.get(0) ?: "Couldn't recognize speech"
+        }
     }
 }
